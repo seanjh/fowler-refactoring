76dd92e314552d1e9a525244eea7b774ec5ae3fd
Sean Herman <seanherman@gmail.com>
2019-06-16 23:36:02 -0400

    Chapter 1 - Price state refactor

--- customers/statement_test.go
+++ customers/statement_test.go
@@ -8,40 +8,40 @@ import (
 
 func TestStatement(t *testing.T) {
 	cases := []struct {
-		customer Customer
+		customer  Customer
 		statement string
 	}{
 		{
 			Customer{"Foo", make([]movies.Rental, 0)},
 			"Rental record for Foo\n" +
-			"Amount owed is 0.000000\n" +
-			"You earned 0 frequent renter points",
+				"Amount owed is 0.000000\n" +
+				"You earned 0 frequent renter points",
 		},
 		{
 			Customer{
 				"Bar",
 				[]movies.Rental{
-					movies.Rental{&movies.Movie{"The Thing", movies.NEW_RELEASE}, 1},
+					movies.Rental{Movie: movies.NewMovie("The Thing", movies.NEW_RELEASE), DaysRented: 1},
 				},
 			},
 			"Rental record for Bar\n" +
-			"\tThe Thing\t3.000000\n" +
-			"Amount owed is 3.000000\n" +
-			"You earned 1 frequent renter points",
+				"\tThe Thing\t3.000000\n" +
+				"Amount owed is 3.000000\n" +
+				"You earned 1 frequent renter points",
 		},
 		{
 			Customer{
 				"Baz",
 				[]movies.Rental{
-					movies.Rental{&movies.Movie{"Paddington 2", movies.CHILDRENS}, 1},
-					movies.Rental{&movies.Movie{"Frozen", movies.CHILDRENS}, 30},
+					movies.Rental{Movie: movies.NewMovie("Paddington 2", movies.CHILDRENS), DaysRented: 1},
+					movies.Rental{Movie: movies.NewMovie("Frozen", movies.CHILDRENS), DaysRented: 30},
 				},
 			},
 			"Rental record for Baz\n" +
-			"\tPaddington 2\t1.500000\n" +
-			"\tFrozen\t42.000000\n" +
-			"Amount owed is 43.500000\n" +
-			"You earned 2 frequent renter points",
+				"\tPaddington 2\t1.500000\n" +
+				"\tFrozen\t42.000000\n" +
+				"Amount owed is 43.500000\n" +
+				"You earned 2 frequent renter points",
 		},
 	}
 
@@ -54,40 +54,40 @@ func TestStatement(t *testing.T) {
 
 func TestHTMLStatement(t *testing.T) {
 	cases := []struct {
-		customer Customer
+		customer  Customer
 		statement string
 	}{
 		{
 			Customer{"Foo", make([]movies.Rental, 0)},
 			"<h1>Rentals for <em>Foo</em></h1><p>\n" +
-			"<p>You owe <em>0.000000</em><p>\n" +
-			"On this rental you earned <em>0</em> frequent renter points<p>",
+				"<p>You owe <em>0.000000</em><p>\n" +
+				"On this rental you earned <em>0</em> frequent renter points<p>",
 		},
 		{
 			Customer{
 				"Bar",
 				[]movies.Rental{
-					movies.Rental{&movies.Movie{"The Thing", movies.NEW_RELEASE}, 1},
+					movies.Rental{Movie: movies.NewMovie("The Thing", movies.NEW_RELEASE), DaysRented: 1},
 				},
 			},
 			"<h1>Rentals for <em>Bar</em></h1><p>\n" +
-			"The Thing: 3.000000<br>\n" +
-			"<p>You owe <em>3.000000</em><p>\n" +
-			"On this rental you earned <em>1</em> frequent renter points<p>",
+				"The Thing: 3.000000<br>\n" +
+				"<p>You owe <em>3.000000</em><p>\n" +
+				"On this rental you earned <em>1</em> frequent renter points<p>",
 		},
 		{
 			Customer{
 				"Baz",
 				[]movies.Rental{
-					movies.Rental{&movies.Movie{"Paddington 2", movies.CHILDRENS}, 1},
-					movies.Rental{&movies.Movie{"Frozen", movies.CHILDRENS}, 30},
+					movies.Rental{Movie: movies.NewMovie("Paddington 2", movies.CHILDRENS), DaysRented: 1},
+					movies.Rental{Movie: movies.NewMovie("Frozen", movies.CHILDRENS), DaysRented: 30},
 				},
 			},
 			"<h1>Rentals for <em>Baz</em></h1><p>\n" +
-			"Paddington 2: 1.500000<br>\n" +
-			"Frozen: 42.000000<br>\n" +
-			"<p>You owe <em>43.500000</em><p>\n" +
-			"On this rental you earned <em>2</em> frequent renter points<p>",
+				"Paddington 2: 1.500000<br>\n" +
+				"Frozen: 42.000000<br>\n" +
+				"<p>You owe <em>43.500000</em><p>\n" +
+				"On this rental you earned <em>2</em> frequent renter points<p>",
 		},
 	}
 
--- movies/movies.go
+++ movies/movies.go
@@ -3,45 +3,104 @@ package movies
 type PriceCode int
 
 const (
-	CHILDRENS PriceCode = 2
-	REGULAR PriceCode = 0
+	CHILDRENS   PriceCode = 2
+	REGULAR     PriceCode = 0
 	NEW_RELEASE PriceCode = 1
 )
 
+type Price interface {
+	Code() PriceCode
+	Charge(daysRented int) float64
+	FrequentRenterPoints(daysRented int) int
+}
+
+type childrensPrice struct{}
+
+func (p *childrensPrice) Code() PriceCode { return CHILDRENS }
+
+func (p *childrensPrice) Charge(daysRented int) float64 {
+	result := 1.5
+	if daysRented > 3 {
+		result += (float64)(daysRented-3) * 1.5
+	}
+	return result
+}
+
+func (p *childrensPrice) FrequentRenterPoints(daysRented int) int { return 1 }
+
+type newReleasePrice struct{}
+
+func (p *newReleasePrice) Code() PriceCode { return NEW_RELEASE }
+
+func (p *newReleasePrice) Charge(daysRented int) float64 {
+	return float64(daysRented) * 3.0
+}
+
+func (p *newReleasePrice) FrequentRenterPoints(daysRented int) int {
+	// add bonus for a two day new release rental
+	if daysRented > 1 {
+		return 2
+	}
+	return 1
+}
+
+type regularPrice struct{}
+
+func (p *regularPrice) Code() PriceCode { return REGULAR }
+
+func (p *regularPrice) Charge(daysRented int) float64 {
+	result := 2.0
+	if daysRented > 2 {
+		result += (float64)(daysRented-2) * 1.5
+	}
+	return result
+}
+
+func (p *regularPrice) FrequentRenterPoints(daysRented int) int { return 1 }
+
 type Movie struct {
 	Title string
-	PriceCode PriceCode
+	price Price
+}
+
+func (m *Movie) PriceCode() PriceCode {
+	return m.price.Code()
+}
+
+func NewMovie(title string, priceCode PriceCode) *Movie {
+	m := &Movie{Title: title}
+	m.setPriceCode(priceCode)
+	return m
+}
+
+func (m *Movie) setPriceCode(priceCode PriceCode) {
+	switch priceCode {
+	case REGULAR:
+		m.price = new(regularPrice)
+	case CHILDRENS:
+		m.price = new(childrensPrice)
+	case NEW_RELEASE:
+		m.price = new(newReleasePrice)
+	}
+}
+
+func (m *Movie) charge(daysRented int) float64 {
+	return m.price.Charge(daysRented)
+}
+
+func (m *Movie) frequentRenterPoints(daysRented int) int {
+	return m.price.FrequentRenterPoints(daysRented)
 }
 
 type Rental struct {
-	Movie *Movie
+	Movie      *Movie
 	DaysRented int
 }
 
 func (r *Rental) Charge() float64 {
-	result := 0.0
-	switch (r.Movie.PriceCode) {
-	case REGULAR:
-		result += 2
-		if r.DaysRented > 2 {
-			result += (float64)(r.DaysRented - 2) * 1.5
-		}
-	case NEW_RELEASE:
-		result += (float64)(r.DaysRented) * 3
-	case CHILDRENS:
-		result += 1.5
-		if r.DaysRented > 3 {
-			result += (float64)(r.DaysRented - 3) * 1.5
-		}
-	}
-
-	return result
+	return r.Movie.charge(r.DaysRented)
 }
 
 func (r *Rental) FrequentRenterPoints() int {
-	// add bonus for a two dat new release rental
-	if (r.Movie.PriceCode == NEW_RELEASE && r.DaysRented > 1) {
-		return 2
-	}
-	return 1
+	return r.Movie.frequentRenterPoints(r.DaysRented)
 }
